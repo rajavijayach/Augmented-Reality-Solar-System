5522fc4326a7f1d1d446be7e291efeb71d72ab3b
Raja Vijaya Saradhi <rvrajavijay@gmail.com>
2018-03-11 11:45:57 +0530

    Initial Commit

---
+++ Planets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Planets
+//
+//  Created by Vara Prasada Gopi Srinath Samudrala on 3/11/18.
+//  Copyright © 2018 Vara Prasada Gopi Srinath Samudrala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PlanetsTests/PlanetsTests.swift
@@ -0,0 +1,36 @@
+//
+//  PlanetsTests.swift
+//  PlanetsTests
+//
+//  Created by Vara Prasada Gopi Srinath Samudrala on 3/11/18.
+//  Copyright © 2018 Vara Prasada Gopi Srinath Samudrala. All rights reserved.
+//
+
+import XCTest
+@testable import Planets
+
+class PlanetsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
